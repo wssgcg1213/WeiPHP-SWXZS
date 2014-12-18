6d552a7ab4f7477f8f811be757886497d4ab089b
Liuchenling <wssgcg1213@qq.com>
2014-12-18 15:20:39 +0800

    swuser

--- Addons/SwUser/Controller/SwUserController.class.php
+++ Addons/SwUser/Controller/SwUserController.class.php
@@ -4,5 +4,16 @@ namespace Addons\SwUser\Controller;
 use Home\Controller\AddonsController;
 
 class SwUserController extends AddonsController{
+    /**
+     * 显示微信用户列表数据
+     */
+    public function lists() {
+        $this->assign ( 'add_button', false );
+        $this->assign ( 'del_button', false );
+        $this->assign ( 'check_all', false );
 
+        $model = $this->getModel ( 'swuser' );
+
+        parent::common_lists ( $model );
+    }
 }
