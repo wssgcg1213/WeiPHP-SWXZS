41420cd16c001514b0adb597c001f917daae34d0
Liuchenling <wssgcg1213@qq.com>
2014-12-19 17:44:40 +0800

    up

--- Addons/Xytl/Model/WeixinAddonModel.class.php
+++ Addons/Xytl/Model/WeixinAddonModel.class.php
@@ -8,10 +8,17 @@ use Home\Model\WeixinModel;
  */
 class WeixinAddonModel extends WeixinModel{
 	function reply($dataArr, $keywordArr = array()) {
-		$config = getAddonConfig ( 'Xytl' ); // 获取后台插件的配置参数	
+		//$config = getAddonConfig ( 'Xytl' ); // 获取后台插件的配置参数
 		//dump($config);
-
-	} 
+        $url = addons_url ( 'Xytl://Xytl/center' );
+        $articles [0] = array (
+            'Title' => '学院通联',
+            'Description' => '学院各办公室, 学生会以及各班班长、团支书的联系电话及邮箱',
+            //'PicUrl' => '',
+            'Url' => $url
+        );
+        $this->replyNews($articles);
+    }
 
 	// 关注公众号事件
 	public function subscribe() {
