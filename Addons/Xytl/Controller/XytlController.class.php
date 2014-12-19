f8917cb12f2d48cfd819084f68ee947f22ce3e28
Liuchenling <wssgcg1213@qq.com>
2014-12-19 17:14:14 +0800

    up

--- Addons/Xytl/Controller/XytlController.class.php
+++ Addons/Xytl/Controller/XytlController.class.php
@@ -20,16 +20,21 @@ class XytlController extends AddonsController{
     public function center(){
         $_model = M('xytl');
 //        $types = $_model->field('type')->group('type')->select();
-        $data = $_model->select();
+//        $data = M('xytl')->order('order desc')->select();
 //        foreach($types as $k => $item){
 //            $types[$k]['field'] = array();
 //            foreach($data as $i){
-//                if($i['type'] == $types[$k]['type']){
+//                if($i['type'] == $item['type']){
 //                    array_push($types[$k]['field'], $i);
 //                }
 //            }
 //        }
-
+        $data = M('xytl')->order('od DESC')->select();
+        $res = array();
+        foreach ($data as $v) {
+            $res[$v['type']][] = $v;
+        }
         echo json_encode($data);
+//        echo json_encode($res);
     }
 }
