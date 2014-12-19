947be08ae5f6b3da0088aea681c986c1905027df
Liuchenling <wssgcg1213@qq.com>
2014-12-19 15:26:53 +0800

    up

--- Addons/Xytl/Controller/XytlController.class.php
+++ Addons/Xytl/Controller/XytlController.class.php
@@ -4,5 +4,21 @@ namespace Addons\Xytl\Controller;
 use Home\Controller\AddonsController;
 
 class XytlController extends AddonsController{
+    public function lists(){
+        $this->assign ( 'add_button', true );
+        $this->assign ( 'del_button', true );
+        $this->assign ( 'check_all', false );
 
+        $model = $this->getModel ( 'xytl' );
+
+        parent::common_lists ( $model );
+    }
+
+    /**
+     * center
+     */
+    public function center(){
+        $data = M('xytl')->select();
+        var_dump($data);
+    }
 }
