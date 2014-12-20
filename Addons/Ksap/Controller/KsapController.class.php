8698bae89bfc0efd72afa223d70a0e98d66867d6
Liuchenling <wssgcg1213@qq.com>
2014-12-20 23:36:15 +0800

    add ksap addon

--- Addons/Ksap/Controller/KsapController.class.php
+++ Addons/Ksap/Controller/KsapController.class.php
@@ -4,5 +4,29 @@ namespace Addons\Ksap\Controller;
 use Home\Controller\AddonsController;
 
 class KsapController extends AddonsController{
+    public function center(){
+        $map['openid'] = get_openid();
+        $map['token'] = get_token();
+        $user = M('swuser')->where($map)->find();
+        if(!$user || !$user['user_state']){
+            $url = addons_url('SwUser://SwUser/addBind');
+            return $this->error('请先绑定账户!', $url, 2);
+        }
 
+        if($user['user_type'] == 1){
+            $userCenter = addons_url('SwUser://SwUser/center');
+            $this->error('老师没有考试的.', $userCenter, 3);
+        }else{
+            $config = getAddonConfig ( 'Ksap' );
+            $data = M('ksap')->where(array('school_id' => $user['school_id'], 'term' => $config['term']))->select();
+            $res = array();
+            foreach ($data as $v) {
+                $res[$v['term']][] = $v;
+            }
+            $this->assign('user', $user['real_name']);
+            $this->assign('data', $res);
+            $this->display('center');
+        }
+
+    }
 }
--- Addons/Ksap/Model/WeixinAddonModel.class.php
+++ Addons/Ksap/Model/WeixinAddonModel.class.php
@@ -10,8 +10,39 @@ class WeixinAddonModel extends WeixinModel{
 	function reply($dataArr, $keywordArr = array()) {
 		$config = getAddonConfig ( 'Ksap' ); // 获取后台插件的配置参数	
 		//dump($config);
+        $map['openid'] = get_openid();
+        $map['token'] = get_token();
 
-	} 
+        $user = M('swuser')->where($map)->find();
+
+        $text = $this->formText($user, $config);
+        $this->replyText($text);
+	}
+
+    private function formText($user, $config){
+        if(!$user){
+            return '请先回复绑定并绑定真实信息以使用本功能.';
+        }
+        if($user['user_type'] == 1){
+            return '老师是没有考试安排的0.0';
+        }else{
+            $textArr = array();
+            $userSchedules = M('ksap')->where(array('school_id' => $user['school_id'], 'term' => $config['term']))->select();
+            foreach($userSchedules as $item){
+                $_t = '';
+                $_t .= "科目: {$item['course']},\n";
+                $_t .= "日期: {$item['date']}, \n";
+                $_t .= "时间: {$item['time']},\n";
+                $_t .= "教室: {$item['room']}";
+                array_push($textArr, $_t);
+            }
+            $basic = implode("\n\n", $textArr);
+            $params['openid'] = get_openid();
+            $params['token'] = get_token();
+            $more = "\n\n"."<a href='".addons_url("Ksap://Ksap/center", $params)."'>查看完整记录</a>";
+            return $basic.$more;
+        }
+    }
 
 	// 关注公众号事件
 	public function subscribe() {
