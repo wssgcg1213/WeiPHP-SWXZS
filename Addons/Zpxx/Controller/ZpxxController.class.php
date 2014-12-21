52d4c2ff42dd213329cc82ba1b6c5e9b22a144ec
Liuchenling <wssgcg1213@qq.com>
2014-12-21 16:49:49 +0800

    zpxx test

--- Addons/Zpxx/Controller/ZpxxController.class.php
+++ Addons/Zpxx/Controller/ZpxxController.class.php
@@ -4,5 +4,8 @@ namespace Addons\Zpxx\Controller;
 use Home\Controller\AddonsController;
 
 class ZpxxController extends AddonsController{
-
+    public function test(){
+        $source = wp_file_get_contents('http://job.snnu.edu.cn/index.html');
+        echo $source;
+    }
 }
