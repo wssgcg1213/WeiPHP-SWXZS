7a5a95f57bea96e401faa5563c39eb6383ac5977
Liuchenling <wssgcg1213@qq.com>
2014-12-21 16:43:55 +0800

    xwts test

--- Addons/Xwts/Controller/XwtsController.class.php
+++ Addons/Xwts/Controller/XwtsController.class.php
@@ -4,5 +4,24 @@ namespace Addons\Xwts\Controller;
 use Home\Controller\AddonsController;
 
 class XwtsController extends AddonsController{
+    var $config;
+    function _initialize() {
+        parent::_initialize();
 
+        $controller = strtolower ( _CONTROLLER );
+
+        $res ['title'] = '新闻推送';
+        $res ['url'] = addons_url ( 'CustomReply://CustomReply/lists' );
+        $res ['class'] = $controller == 'customreply' ? 'current' : '';
+        $nav [] = $res;
+
+        $this->assign ( 'nav', $nav );
+
+        $config = getAddonConfig ( 'CustomReply' );
+        $config ['cover_url'] = get_cover_url ( $config ['cover'] );
+        $this->config = $config;
+        $this->assign ( 'config', $config );
+        // dump ( $config );
+        // dump(get_token());
+    }
 }
