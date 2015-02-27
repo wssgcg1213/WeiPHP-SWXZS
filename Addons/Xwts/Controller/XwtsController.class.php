99336bf6d9b9282b42dcc4537d4413457c4d1ecd
Liuchenling <wssgcg1213@qq.com>
2015-02-27 21:30:25 +0800

    fix

--- Addons/Xwts/Controller/XwtsController.class.php
+++ Addons/Xwts/Controller/XwtsController.class.php
@@ -27,9 +27,7 @@ class XwtsController extends AddonsController{
     public function lists($model = null, $page = 0) {
         $model = $this->getModel ( 'custom_reply_news' );
         $templateFile = $this->getAddonTemplate ( $model ['template_list'] );
-        var_dump($model);
-        var_dump($templateFile);
-//        parent::common_lists ( $model, $page, $templateFile );
+        parent::common_lists ( $model, $page, $templateFile );
     }
     public function center(){
         $map['token'] = get_token();
